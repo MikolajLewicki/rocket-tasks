d5ea5691fc8766cac392bb617d427596a93eaa4e
MikolajLewicki <mikolajlewicki00@gmail.com>
2022-07-02 11:06:08 +0200

    created handleChangeTab

--- addon/event.js
+++ addon/event.js
@@ -1,19 +1,28 @@
+const Options = {
+    domain: 'stackoverflow.com',
+}
+
 const contextMenuItem = {
     "id": "sendTask",
     "title": "Oznacz jako sprzedaż",
     "contexts": ["all"]
 };
 
+let link = "";
 
-const handleClick = async () => {
-
-}
-const handleChangeTab = async (e) => {
+///Showing Context Menu only when site url === Options domain
+const handleChangeTab = async () => {
     let queryOptions = { active: true, lastFocusedWindow: true };
     const tab = await chrome.tabs.query(queryOptions);
-    console.log(tab[0].url)
+    link = tab[0].url.slice(8, Options.domain.length+8)
+    link === Options.domain ? chrome.contextMenus.create(contextMenuItem) : chrome.contextMenus.removeAll()
 }
 chrome.tabs.onActivated.addListener(handleChangeTab)
 
-chrome.contextMenus.create(contextMenuItem)
+///handling Context Menu action
+
+const handleClick = async () => {
+    console.log('it works')
+}
+
 chrome.contextMenus.onClicked.addListener(handleClick)
