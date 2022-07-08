6b4766d4f53f449d1bfbf21b0834a3e2ae614193
MikolajLewicki <mikolajlewicki00@gmail.com>
2022-07-08 10:57:00 +0200

    stats backend

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -4,113 +4,139 @@ import statsStore from '../../zustand/statsStore'
 import { motion } from 'framer-motion'
 import Chart from 'react-apexcharts'
 const Stats = () => {
-    const content = statsStore(state => state.content)
-    const [chartsData, setChartsData] = useState({
-        PieChart1: {
-            options: {
-                labels: ['Apple', 'Mango', 'Orange', 'Carrot', 'Melon', 'Strawberry'],
-                dataLabels: {
-                    formatter: (val, opts) => opts.w.config.series[opts.seriesIndex]
-                },
-                colors: ['#C1BDDB', '#D7C4F2', '#DABCE8', '#C4CAF2', '#BCCDE8'],
-            },
-            series: [44, 55, 42, 12, 16, 89],
+    const status = statsStore(state => state.status)
+    const chartsContent = statsStore(state => state.chartsContent)
+    const initialState = {
+      PieChart1: {
+        options: {
+          labels: ["Nowe zadania", "W trakcie pracy", "Zakończone"],
+          dataLabels: {
+              formatter: (val, opts) => opts.w.config.series[opts.seriesIndex]
           },
-        PieChart2: {
-            options: {
-                labels: ['Apple', 'Mango', 'Orange', 'Carrot', 'Melon', 'Strawberry'],
-                dataLabels: {
-                    formatter: (val, opts) => opts.w.config.series[opts.seriesIndex]
-                },
-                colors: ['#C1BDDB', '#D7C4F2', '#DABCE8', '#C4CAF2', '#BCCDE8'],
-            },
-            series: [44, 55, 42, 12, 16, 89],
+          colors: ['#C1BDDB', '#D7C4F2', '#DABCE8', '#C4CAF2', '#BCCDE8'],
+      },
+      series: status === "showing tasks" ? [chartsContent.pieChart1.new.length, chartsContent.pieChart1.work.length, chartsContent.pieChart1.end.length] : [],
+      },
+      PieChart2: {
+        options: {
+          labels: ["Nowe zadania", "W trakcie pracy", "Zakończone"],
+          dataLabels: {
+              formatter: (val, opts) => opts.w.config.series[opts.seriesIndex]
           },
-        BarChart1: {
-            series: [{
-              name: 'Website Blog',
-              type: 'column',
-              data: [44, 50, 41, 67, 22, 13, 21, 35, 52, 32, 57, 10]
-            }, {
-              name: 'Social Media',
-              type: 'line',
-              data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-            }],
-            options: {
-            colors: ['#C1BDDB', '#BCCDE8', '#DABCE8', '#C4CAF2', '#D7C4F2'],
-              stroke: {
-                width: [0, 4]
-              },
-              dataLabels: {
-                enabled: true,
-              },
-              labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
-              xaxis: {
-                type: 'datetime'
-              }
-            },
+          colors: ['#C1BDDB', '#D7C4F2', '#DABCE8', '#C4CAF2', '#BCCDE8'],
+      },
+      series: status === "showing tasks" ? [chartsContent.pieChart2.new.length, chartsContent.pieChart2.work.length, chartsContent.pieChart2.end.length] : [],
+      },
+      BarChart1: {
+        series: [{
+          name: '1 Zakres Dat',
+          type: 'column',
+          data: status === "showing tasks" ? chartsContent.barChart1.chartData.map(item => item.range1) : []
+        }, {
+          name: '2 Zakres Dat',
+          type: 'line',
+          data: status === "showing tasks" ? chartsContent.barChart1.chartData.map(item => item.range2) : []
+        }],
+        options: {
+        colors: ['#C1BDDB', '#BCCDE8', '#DABCE8', '#C4CAF2', '#D7C4F2'],
+          stroke: {
+            width: [0, 4]
+          },
+          dataLabels: {
+            enabled: true,
+          },
+          labels: status === "showing tasks" ? chartsContent.barChart1.chartData.map(item => item.date) : [],
+          xaxis: {
+            type: 'datetime'
+          }
         },
-        BarChart2: {
-            series: [{
-              name: 'Website Blog',
-              type: 'column',
-              data: [44, 50, 41, 67, 22, 13, 21, 35, 52, 32, 57, 10]
-            }, {
-              name: 'Social Media',
-              type: 'line',
-              data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-            }],
-            options: {
-            colors: ['#C1BDDB', '#BCCDE8', '#DABCE8', '#C4CAF2', '#D7C4F2'],
-              stroke: {
-                width: [0, 4]
-              },
-              dataLabels: {
-                enabled: true,
-              },
-              labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
-              xaxis: {
-                type: 'datetime'
-              }
-            },
+      },
+      BarChart2: {
+        series: [{
+          name: '1 Zakres Dat',
+          type: 'column',
+          data: status === "showing tasks" ? chartsContent.barChart2.chartData.map(item => item.range1) : []
+        }, {
+          name: '2 Zakres Dat',
+          type: 'line',
+          data: status === "showing tasks" ? chartsContent.barChart2.chartData.map(item => item.range2) : []
+        }],
+        options: {
+        colors: ['#C1BDDB', '#BCCDE8', '#DABCE8', '#C4CAF2', '#D7C4F2'],
+          stroke: {
+            width: [0, 4]
+          },
+          dataLabels: {
+            enabled: true,
+          },
+          labels: status === "showing tasks" ? chartsContent.barChart2.chartData.map(item => item.date) : [],
+          xaxis: {
+            type: 'datetime'
+          }
         },
-        BarChart3: {
-            series: [{
-              name: 'Website Blog',
-              type: 'column',
-              data: [44, 50, 41, 67, 22, 13, 21, 35, 52, 32, 57, 10]
-            }, {
-              name: 'Social Media',
-              type: 'line',
-              data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-            }],
-            options: {
-            colors: ['#C1BDDB', '#BCCDE8', '#DABCE8', '#C4CAF2', '#D7C4F2'],
-              stroke: {
-                width: [0, 4]
-              },
-              dataLabels: {
-                enabled: true,
-              },
-              labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
-              xaxis: {
-                type: 'datetime'
-              }
-            },
+      },
+      BarChart3: {
+        series: [{
+          name: '1 Zakres Dat',
+          type: 'column',
+          data: status === "showing tasks" ? chartsContent.barChart3.chartData.map(item => item.range1) : []
+        }, {
+          name: '2 Zakres Dat',
+          type: 'line',
+          data: status === "showing tasks" ? chartsContent.barChart3.chartData.map(item => item.range2) : []
+        }],
+        options: {
+        colors: ['#C1BDDB', '#BCCDE8', '#DABCE8', '#C4CAF2', '#D7C4F2'],
+          stroke: {
+            width: [0, 4]
+          },
+          dataLabels: {
+            enabled: true,
+          },
+          labels: status === "showing tasks" ? chartsContent.barChart3.chartData.map(item => item.date) : [],
+          xaxis: {
+            type: 'datetime'
+          }
         },
-    })
+      }
+    }
+    const initialState2 = {
+      BarChart1: {
+        range1Length: status === "showing tasks" ? chartsContent.barChart1.data.range1Length : "",
+        range2Length: status === "showing tasks" ? chartsContent.barChart1.data.range2Length : "",
+        difference: status === "showing tasks" ? chartsContent.barChart1.data.difference : "",
+      },
+      BarChart2: {
+        range1Ang: status === "showing tasks" ? chartsContent.barChart2.data.range1Ang : "",
+        range2Avg: status === "showing tasks" ? chartsContent.barChart2.data.range2Avg : "",
+        difference: status === "showing tasks" ? chartsContent.barChart2.data.difference : "",
+      },
+      BarChart3: {
+        range1Ang: status === "showing tasks" ? chartsContent.barChart3.data.range1Ang : "",
+        range2Avg: status === "showing tasks" ? chartsContent.barChart3.data.range2Avg : "",
+        difference: status === "showing tasks" ? chartsContent.barChart3.data.difference : "",
+      },
+    }
+    const [chartsData, setChartsData] = useState(initialState)
+    const [descriptionData, setDescriptionData] = useState(initialState2)
+    
+    useEffect(() => {
+      setChartsData(initialState)
+      setDescriptionData(initialState2)
+    }, [chartsContent])
     return(
-        <>{content.length !== 0 ? 
+        <>{status === "no filters" ? 
             <motion.div initial={{opacity: 0, scale: 0.75}} animate={{opacity: 1, scale: 1, transition: {delay: 0.1}}}  className={styles.noMatchingResults}>
                 <h2>Porównaj swoje wyniki!</h2>
                 <p>Dostosuj filtry aby wyświetlić odpowiednie Statystyki</p>
-            </motion.div> : <> {content === "no tasks" ? 
+            </motion.div> : <> {status === "no tasks" ? 
                 <motion.div initial={{opacity: 0, scale: 0.75}} animate={{opacity: 1, scale: 1, transition: {delay: 0.3}}}  className={styles.noMatchingResults}>
                     <h1>Ooops!</h1>
                     <h2>Brak pasujących zadań</h2>
                     <p>Dostosuj filtry aby znaleźć inne wyniki</p></motion.div> : 
                 <div className={styles.wrapper}>
                     <div className={styles.title}> Statystyki z okresu </div>
+                    <motion.div initial={{y: 20, opacity: 0}} animate={{y: 0, opacity: 1}}>
                     <div className={styles.sectionTitle}>Podział Zadań</div>
                     <div className={styles.chartContainer}>
                         <div style={{width: '50%'}}>
@@ -126,15 +152,15 @@ const Stats = () => {
                         <Chart options={chartsData.BarChart1.options} series={chartsData.BarChart1.series} type="line" width="100%" height={320} />
                         </div>
                         <div className={styles.descriptionContainer}>
-                            <div className={styles.value}>+22%</div>
-                            <div className={styles.description}>Ilość przydzielonych zadańn: 11 (7)</div>
+                            <div className={styles.value}>{descriptionData.BarChart1.difference}%</div>
+                            <div className={styles.description}>Ilość przydzielonych zadańn: {descriptionData.BarChart1.range1Length} ({descriptionData.BarChart1.range2Length})</div>
                         </div>
                     </div>
                     <div className={styles.sectionTitle}>Rozpoczęcie pracy nad zadaniami</div>
                     <div className={styles.chartContainer}>
                         <div className={styles.descriptionContainer}>
-                            <div className={styles.value}>+22%</div>
-                            <div className={styles.description}>Średni czas rozpoczęcia pracy: 2dni (1.3 dnia) </div>
+                            <div className={styles.value}>{descriptionData.BarChart2.difference}%</div>
+                            <div className={styles.description}>Średni czas rozpoczęcia pracy: {descriptionData.BarChart2.range1Ang}h ({descriptionData.BarChart2.range2Avg}h) </div>
                         </div>
                         <div style={{width: '75%'}}>
                         <Chart options={chartsData.BarChart2.options} series={chartsData.BarChart2.series} type="line" width="100%" height={320} />
@@ -146,10 +172,11 @@ const Stats = () => {
                         <Chart options={chartsData.BarChart3.options} series={chartsData.BarChart3.series} type="line" width="100%" height={320} />
                         </div>
                         <div className={styles.descriptionContainer}>
-                            <div className={styles.value}>+22%</div>
-                            <div className={styles.description}>Średni czas pracy: 2dni (1.3 dnia)</div>
+                            <div className={styles.value}>{descriptionData.BarChart3.difference}%</div>
+                            <div className={styles.description}>Średni czas pracy: {descriptionData.BarChart3.range1Ang}h ({descriptionData.BarChart3.range2Avg}h)</div>
                         </div>
                     </div>
+                </motion.div>
                 </div>}
             </>}</>
         )
--- client/src/zustand/statsStore.js
+++ client/src/zustand/statsStore.js
@@ -3,11 +3,163 @@ import * as api from '../api/index'
 import { devtools } from 'zustand/middleware'
 
 const store = (set) => ({
-    content: [],
+    chartsContent: {},
+    status: "no filters",
     filters: {},
-    getContent: (filters) => {
-        console.log('im here')
-        console.log(filters)
+    getContent: async (filters) => {
+        let range1 = (await api.getTasks()).data.result;
+        let range2 = (await api.getTasks()).data.result;
+
+
+        //// Checking is user in filters
+        range1 = range1.filter(item => { 
+            for(let i = 0; i < filters.users.length; i++){
+                if(item.assignedFor === filters.users[i]){
+                    return item
+                }
+            }
+        })
+        range2 = range2.filter(item => { 
+            for(let i = 0; i < filters.users.length; i++){
+                if(item.assignedFor === filters.users[i]){
+                    return item
+                }
+            }
+        })
+
+        /// Checking is task fits in date filters
+        const checkDate = (date1, date2, date3) => {
+            const from = new Date(date1.slice(0,4), parseInt(date1.slice(6,7)) - 1, date1.slice(8,10), 0, 0).getTime()
+            const to = new Date(date2.slice(0,4), parseInt(date2.slice(6,7)) - 1, date2.slice(8,10), 23, 59).getTime()
+            const check = new Date(date3.slice(0,4), parseInt(date3.slice(6,7)) - 1, date3.slice(8,10), date3.slice(11, 13)).getTime()
+            if(check > from && check < to){
+                return true
+            }else{
+                return false
+            }
+        }
+
+        range1 = range1.filter(item => { 
+            if(checkDate(filters.range1Start, filters.range1End, item.dateOfCreation )){
+                return item
+            }
+        }) 
+        range2 = range2.filter(item => { 
+            if(checkDate(filters.range2Start, filters.range2End, item.dateOfCreation)){
+                return item
+            }
+        }) 
+        ///Making pie chart ready objects
+        const makePieChart = (obj) => {
+            let result = {
+                new: obj.filter(item => item.status === "new"),
+                work: obj.filter(item => item.status === "work"),
+                end: obj.filter(item => item.status === "end"),
+            }
+            return result
+        }
+
+
+        ///Making bar chart ready objects
+        const makeBarChart = (obj1, obj2, toCheck) => {
+            let result = {}
+            let endresult = {}
+            obj1.map(item => {
+                if(typeof(item[toCheck]) !== "undefined"){
+                    result = {
+                        ...result,
+                        [item[toCheck].slice(0,10)]: {date: `${item[toCheck].slice(0,10)}`, 
+                        range1: typeof(result[item[toCheck].slice(0,10)]) !== "undefined" ? result[item[toCheck].slice(0,10)].range1 + 1 : 1,
+                        range2: 0,
+                    }
+        
+                    }
+                }
+            })
+            obj2.map(item => {
+                if(typeof(item[toCheck]) !== "undefined"){
+                    if(typeof(result[item[toCheck].slice(0,10)]) !== "undefined"){
+                            result = {
+                                ...result,
+                                [item[toCheck].slice(0,10)]: {date: `${item[toCheck].slice(0,10)}`,
+                                 range1: result[item[toCheck].slice(0,10)].range1,
+                                 range2: typeof(result[item[toCheck].slice(0,10)].range2) !== "undefined" ? result[item[toCheck].slice(0,10)].range2 + 1 : 1  
+                            }}
+                    }else{
+                        result = {
+                            ...result,
+                            [item[toCheck].slice(0,10)]: {date: `${item[toCheck].slice(0,10)}`, range1: 0, range2: 1}
+                        }
+                    }
+            }})
+            const getDifference = (num1, num2) => {
+                let result = Math.round(((((num1 - num2) / num2)* 100)* 10) / 10)
+                if(result !== Infinity){
+                    return result
+                }else{
+                    return '∞'
+                }
+            }
+            if(toCheck === "dateOfCreation"){ ///get stats about numbers of tasks assigend
+                endresult = {
+                    chartData: Object.values(result),
+                    data: {
+                        range1Length: range1.length,
+                        range2Length: range2.length,
+                        difference: getDifference(range1.length, range2.length),
+                    }
+                }
+            }else{ ///get stats about time of work
+                
+                const getAvgTime = (obj) => {
+                    let SumTime = 0
+                    let numOfItems = 0
+                    obj.map(item => {
+                        if(typeof(item[toCheck]) !== "undefined"){
+                        SumTime = SumTime + (new Date(item[toCheck]).getTime() - new Date(item.dateOfCreation).getTime())
+                        numOfItems++
+                    }})
+
+                    if(numOfItems !== 0){
+                        let miliseconds =  Math.round((SumTime / numOfItems * 10) / 10)
+                        let hours = Math.floor(Math.floor(Math.floor(miliseconds / 1000) / 60) / 60)
+                        if(hours !== 0){
+                            return hours
+                        }
+                        return 1
+                    }else{
+                        return 0
+                    }
+
+                }
+
+                endresult = {
+                    chartData: Object.values(result),
+                    data: {
+                        range1Ang: getAvgTime(range1),
+                        range2Avg: getAvgTime(range2),
+                        difference: getDifference(getAvgTime(range1), getAvgTime(range2)),
+                    }
+                }
+            }
+
+            
+            return endresult
+        }
+
+
+        if(range1.length === 0 && range2.length === 0){
+            set((state) => ({status: "no tasks"}))
+        }else{
+            set((state) => ({chartsContent: {
+                pieChart1: makePieChart(range1),
+                pieChart2: makePieChart(range2),
+                barChart1: makeBarChart(range1, range2, "dateOfCreation"),
+                barChart2: makeBarChart(range1, range2, "startOfWork"),
+                barChart3: makeBarChart(range1, range2, "endOfWork"),
+            }}))
+            set((state) => ({status: "showing tasks"}))
+        }
     }
 })
 
