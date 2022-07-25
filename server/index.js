c4195cff7b90dcf96ef38025c4ae7097b0479724
MikolajLewicki <mikolajlewicki00@gmail.com>
2022-07-25 08:12:09 +0200

    deleted docker

--- server/index.js
+++ server/index.js
@@ -5,13 +5,11 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import usersRouter from './routes/users.js'
 import tasksRouter from './routes/tasks.js'
-import * as path from 'path'
 
 dotenv.config()
 const app = express()
-
 mongoose.connect(process.env.CONNECTION_URL)
-.then(() => app.listen(process.env.PORT, () => console.log(`server running on port ${process.env.PORT}`) ))
+.then(() => app.listen(process.env.PORT || 5000, () => console.log(`server running on port ${process.env.PORT}`) ))
 .catch((err) => console.log(err))
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
@@ -20,13 +18,4 @@ app.use(cors())
 app.use('/api/users', usersRouter)
 app.use('/api/tasks', tasksRouter)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, '../client/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
-    })
-}else {
-    app.get('/', (req, res) => res.send('Please set to production'))
-}
 
